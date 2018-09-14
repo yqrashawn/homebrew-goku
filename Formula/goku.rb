3a279ef69f19a4036ade22bcdbea3fce317f53e5
yqrashawn <namy.19@gmail.com>
2018-09-14 18:02:58 +0800

    add formula

---
+++ Formula/goku.rb
@@ -0,0 +1,45 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+class Gokurakujoudo < Formula
+  depends_on "watchexec"
+  desc "karabiner configurator"
+  homepage "https://github.com/yqrashawn/GokuRakuJoudo"
+  url "https://github.com/yqrashawn/GokuRakuJoudo/releases/download/v0.1.3/goku"
+  sha256 "ff31ad1dcd0c4cd50783af8554351aea3c6968d0b38e1d17fecb164421f38221"
+
+  def install
+    bin.install "goku"
+  end
+
+  plist_options :manual => true
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>/usr/local/opt/watchexec/bin/watchexec</string>
+        <string>--restart</string>
+        <string>--watch</string>
+        <string>#{ENV["HOME"]}/.config/karabiner.edn</string>
+        <string>/usr/local/opt/gokurakujoudo/bin/goku</string>
+      </array>
+      <key>StandardErrorPath</key>
+      <string>#{ENV["HOME"]}/Library/Logs/goku/error.log</string>
+      <key>StandardOutPath</key>
+      <string>#{ENV["HOME"]}/Library/Logs/goku/output.log</string>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system "#{bin}/goku", "--help"
+  end
+end
